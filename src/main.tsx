a9f3c5106db700c8ba1ee90e561154336872d3dd
CharlesLawson <charles.thomas.lawson@gmail.com>
2025-09-21 14:55:54 -0400

    commented out outputs.json stuff in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Amplify } from 'aws-amplify';
-import outputs from "../amplify_outputs.json";
+//import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
+//Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
